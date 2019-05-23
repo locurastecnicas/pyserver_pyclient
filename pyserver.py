9013b50b104b736f5f4f9a1a4fe38331b8332511
Eduardo Izaguirre Pazos <eizaguirrep@gmail.com>
2019-05-23 21:36:41 +0200

    Threading del servidor. Multiples clientes simultaneos ya soportados.

--- pyserver.py
+++ pyserver.py
@@ -5,11 +5,31 @@ import time
 import socket
 import sys
 import signal
+import threading
 
 # Direccion IP del servidor
 HOST='127.0.0.1'
 # Puerto de escucha del servidor.
 PORT=60000
+
+class echo_server(threading.Thread):
+  def __init__(self, connected_socket, client_addr):
+    threading.Thread.__init__(self)
+    self.chat_socket=connected_socket
+    self.remote_addr=client_addr
+    print("Iniciado thread de conversacion.")
+
+  def run(self):
+    # Recibir datos del cliente.
+    while True:
+        recData=self.chat_socket.recv(1024)
+        print("La direccion remota es ", self.remote_addr )
+        print("Los datos recibidos son ", recData)
+        # Enviar datos al cliente.
+        self.chat_socket.send(b'Hola cliente, yo soy el servidor.')
+        if not recData:
+            break
+    self.chat_socket.close()
 
 def control_signal(signal_control, signal_handler):
   print("Stopping pyerver. Please wait....")
@@ -48,16 +68,19 @@ ServerSocket.listen(10)
 # Lanzamos el metodo accept sobre el socket. Esto bloquea el codigo hasta que se establezca
 # una conexion. Este metodo devuelve una pareja conn, addr; siendo conn un nuevo objecto socket
 # para la nueva conexion y addr la direccion remota que se ha conectado.
-connSocket,remoteAddr=ServerSocket.accept()  
-# Recibir datos del cliente.
 while True:
-    recData=connSocket.recv(1024)
-    print("La direccion remota es ", remoteAddr )
-    print("Los datos recibidos son ", recData)
-    # Enviar datos al cliente.
-    connSocket.send(b'Hola cliente, yo soy el servidor.')
-    if not recData:
-        break
+    connSocket,remoteAddr=ServerSocket.accept()  
+    chat=echo_server(connSocket,remoteAddr) 
+    chat.start()
+# Recibir datos del cliente.
+#while True:
+#    recData=connSocket.recv(1024)
+#    print("La direccion remota es ", remoteAddr )
+#    print("Los datos recibidos son ", recData)
+#    # Enviar datos al cliente.
+#    connSocket.send(b'Hola cliente, yo soy el servidor.')
+#    if not recData:
+#        break
 
-connSocket.close()
 ServerSocket.close()
+
